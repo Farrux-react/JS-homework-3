b78f787b687c1b1353a9158cfdf3f58ca3b5be08
Farrux-react <farruxusarbayev@gmail.com>
2023-05-26 16:26:39 +0300

    js homework 3

---
+++ main.js
@@ -0,0 +1,132 @@
+//1-masala
+/*
+let a = 5;
+let b = 2;
+let c;
+
+c = a - b;
+
+console.log(c);
+*/
+
+//2-masala
+/*
+let str = prompt("Str kiriting");
+let index = prompt("Son kiriting");
+
+console.log(str[index]);
+*/
+
+//3-masala
+/*
+let str = prompt("STR");
+
+if (str[0] == "h") {
+    if (str[1] == "i") {
+        console.log(true);
+    }
+} else {
+    console.log(false);
+}
+*/
+
+//4-masala
+
+//5-masala
+/*
+let a = 10;
+
+if (a % 3 == 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (a % 5 == 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
+
+//6-masala
+/*
+let a = 10;
+let b = 55;
+
+
+if (a > 0 && a <= 100 && b > 0 && b <= 100) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
+
+//7-masala
+/*
+let a = 3;
+let b = 2;
+
+if (a > b) {
+    console.log("1 katta");
+}
+
+if (a < b) {
+    console.log("2 katta");
+}
+
+if (a == b) {
+    console.log("Teng");
+}
+*/
+
+//8-masala
+/*
+let a = 40;
+let b = 11;
+let c;
+
+c = a + b;
+
+if (c >= 30 && c <= 50) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
+
+//9-masala
+/*
+let a = 90;
+let b = 20;
+
+if(a % b == 10){
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
+
+//10-masala
+/*
+let a = 10;
+let b = 10;
+
+if (a == b) {
+    console.log((a + b) * 2);
+} else {
+    console.log(a + b);
+}
+*/
+
+//11-masala
+/*
+let a = 5;
+let b = 5;
+
+if (a == 10 || b == 10 || a + b == 10) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
